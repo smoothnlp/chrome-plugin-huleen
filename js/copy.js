828e2ecb2a40f94690c109b5921585c35170699e
Cacaci <logoweiyi@gmail.com>
2021-11-05 13:49:46 +0800

    feat: another solution

--- js/copy.js
+++ js/copy.js
@@ -3,42 +3,36 @@ var copyCommand = 'Control,Shift,C'
 chrome.extension.sendMessage(
   { "type" : "config" },
   function () {
+    // Tips: 两种方法都不支持 http
+    // 1. 往剪贴板写入多种格式
     // document.body.addEventListener('copy', async function (event) {
     //   event.preventDefault()
     //   var pageUrl = window.location.href
     //   var encodedUrl = encodeURI(pageUrl.split('#')[0])
     //   var copyText = window.getSelection().toString();
     //   encodedUrl += '#:~:text=' + copyText
-    //   // event.clipboardData.setData('text/html', `
-    //   //     <a
-    //   //       href="${encodeURIComponent(encodedUrl)}"
-    //   //       title="${document.title}" 
-    //   //       id="${guid()}"
-    //   //       type="smartlink-inline"
-    //   //       data-target-id="${encodeURIComponent(encodedUrl)}"
-    //   //       data-target-type="webs"
-    //   //       data-target-origin-text="${copyText}">${copyText}</a>
-    //   //     `);
-    //   // event.clipboardData.setData('text/plain', copyText)
 
-    //   console.log(await navigator.clipboard.read('text/html'))
-
-    //   // var cliboardHTMLData = new Blob([`<a
-    //   //   href="${encodeURIComponent(encodedUrl)}"
-    //   //   title="${document.title}" 
-    //   //   id="${guid()}"
-    //   //   type="smartlink-inline"
-    //   //   data-target-id="${encodeURIComponent(encodedUrl)}"
-    //   //   data-target-type="webs"
-    //   //   data-target-origin-text="${copyText}">${copyText}</a>`], { type: 'text/html' })
-    //   // var cliboardTextData = new Blob([copyText], { type: 'text/plain' })
-    //   // await navigator.clipboard.write([
-    //   //   new ClipboardItem({
-    //   //     'text/html': cliboardHTMLData,
-    //   //     'text/plain': cliboardTextData
-    //   //   })
-    //   // ]);
+    //   var cliboardHTMLData = new Blob([`<a
+    //     href="${encodedUrl}"
+    //     title="${document.title}" 
+    //     id="${guid()}"
+    //     type="smartlink-inline"
+    //     data-target-id="${encodedUrl}"
+    //     data-target-type="webs"
+    //     data-target-origin-text="${copyText}">${copyText}</a>`], { type: 'text/html' })
+    //   var cliboardTextData = new Blob([copyText], { type: 'text/plain' })
+    //   await navigator.clipboard.write([
+    //     new ClipboardItem({
+    //       'text/html': cliboardHTMLData,
+    //       'text/plain': cliboardTextData
+    //     })
+    //   ]).then(() => {
+    //     console.info('write successfully')
+    //   }).catch(error => {
+    //     console.error(error)
+    //   });
     // })
+    // 2. 发请求传递原始数据
     document.body.addEventListener('keydown', function (event) {
       if ((event.metaKey && event.keyCode === 67) || (event.ctrlKey && event.keyCode === 67)) {
         var copyText = window.getSelection().toString();
